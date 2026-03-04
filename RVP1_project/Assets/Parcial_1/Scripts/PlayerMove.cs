2a20e43270595cfa54bef811ef97d872c2460b56
SebMen22 <sebastian.menesesm@uao.edu.co>
2026-03-04 15:20:39 -0500

    Actualizacion V1.3
    Se hace el script basico de PlayerMove, con teclado, wasd y las flechas del teclado y el gamepad playStationCOntroller

--- RVP1_project/Assets/Parcial_1/Scripts/PlayerMove.cs
+++ RVP1_project/Assets/Parcial_1/Scripts/PlayerMove.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private NIS inputActions;      
+    private Vector2 moveInput;     
+
+    public float speed = 5f;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        inputActions = new NIS();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        inputActions.Enable();
+
+       
+        inputActions.PlayerMove.Caminar.performed += OnMove;
+        inputActions.PlayerMove.Caminar.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.PlayerMove.Caminar.performed -= OnMove;
+        inputActions.PlayerMove.Caminar.canceled -= OnMove;
+        inputActions.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * speed;
     }
 }
