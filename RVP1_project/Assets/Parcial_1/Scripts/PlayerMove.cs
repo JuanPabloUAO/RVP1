9fa392706c0398eb729f67c766e393387ee8d5a6
SebMen22 <sebastian.menesesm@uao.edu.co>
2026-03-05 23:47:59 -0500

    Actualizacion V3
    se agrega el salto y todo queda funcionando correctamente.

--- RVP1_project/Assets/Parcial_1/Scripts/PlayerMove.cs
+++ RVP1_project/Assets/Parcial_1/Scripts/PlayerMove.cs
@@ -5,13 +5,17 @@ public class PlayerMove : MonoBehaviour
 {
     public int jugador = 1;
     public float velocidad = 5f;
+    public float fuerzaSalto = 7f;
 
     private NIS controles;
     private Vector2 movimiento;
+    private Rigidbody2D rb;
+    private bool enSuelo = true;
 
     void Awake()
     {
         controles = new NIS();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void OnEnable()
@@ -22,30 +26,62 @@ public class PlayerMove : MonoBehaviour
         {
             controles.PlayerMove.CaminarP1.performed += ctx => movimiento = ctx.ReadValue<Vector2>();
             controles.PlayerMove.CaminarP1.canceled += ctx => movimiento = Vector2.zero;
+
+            controles.PlayerMove.SaltarP1.performed += ctx => Saltar();
         }
 
         if (jugador == 2)
         {
             controles.PlayerMove.CaminarP2.performed += ctx => movimiento = ctx.ReadValue<Vector2>();
             controles.PlayerMove.CaminarP2.canceled += ctx => movimiento = Vector2.zero;
+
+            controles.PlayerMove.SaltarP2.performed += ctx => Saltar();
         }
 
         if (jugador == 3)
         {
             controles.PlayerMove.CaminarP3.performed += ctx => movimiento = ctx.ReadValue<Vector2>();
             controles.PlayerMove.CaminarP3.canceled += ctx => movimiento = Vector2.zero;
+
+            controles.PlayerMove.SaltarP3.performed += ctx => Saltar();
         }
 
         if (jugador == 4)
         {
             controles.PlayerMove.CaminarP4.performed += ctx => movimiento = ctx.ReadValue<Vector2>();
             controles.PlayerMove.CaminarP4.canceled += ctx => movimiento = Vector2.zero;
+
+            controles.PlayerMove.SaltarP4.performed += ctx => Saltar();
+        }
+
+        if (jugador == 5)
+        {
+            controles.PlayerMove.CaminarP5.performed += ctx => movimiento = ctx.ReadValue<Vector2>();
+            controles.PlayerMove.CaminarP5.canceled += ctx => movimiento = Vector2.zero;
+
+            controles.PlayerMove.SaltarP5.performed += ctx => Saltar();
         }
     }
 
     void Update()
     {
-        Vector3 dir = new Vector3(movimiento.x, movimiento.y, 0);
+        Vector3 dir = new Vector3(movimiento.x, 0, 0);
         transform.Translate(dir * velocidad * Time.deltaTime);
+    }
+
+    void Saltar()
+    {
+        if (!enSuelo) return;
+
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
+        enSuelo = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground") || col.gameObject.CompareTag("Player"))
+        {
+            enSuelo = true;
+        }
     }
 }
