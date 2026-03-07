3ffd7ea05c940a4bfee9a9f34579f413079700f5
SebPla82 <plazasebastian14@gmail.com>
2026-03-07 00:23:02 -0500

    sonidos level 1

--- RVP1_project/Assets/Parcial_1/Scripts/Props/Trampoline.cs
+++ RVP1_project/Assets/Parcial_1/Scripts/Props/Trampoline.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class Trampoline : MonoBehaviour
 {
     public Animator animator;
-
     public float jumpForce = 2f;
+
+    public AudioSource audioSource;
+    public AudioClip sonidoTrampolin;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity = (Vector2.up * jumpForce);
+            collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity = Vector2.up * jumpForce;
+
             animator.Play("JumpTrampoline");
+
+            if (sonidoTrampolin != null)
+            {
+                audioSource.PlayOneShot(sonidoTrampolin);
+            }
         }
     }
 }
